24142eb0dca7b24639ab35e3b567a52213f18ed5
Velho008 <gabrielvelho08@gmail.com>
2026-05-24 19:54:27 -0300

    fix: tailwind.config corretamente configurado, fontes adicionadas no index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "#0F172A",
+        surface: "#1E293B",
+
+        text: {
+          primary: "#F8FAFC",
+          secondary: "#94A3B8",
+        },
+
+        brand: {
+          primary: "#0F766E", // destaque
+        },
+
+        success: "#166534",
+        danger: "#831843",
+      },
+
+      fontFamily: {
+        display: ["IBM Plex Sans", "sans-serif"],
+        body: ["Inter", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
